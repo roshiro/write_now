33673f7a4928f040e02e1e0ca3871bf025429461
Rafael Oshiro <rafael.oshiro@sage.com>
2016-12-29 17:04:15 +0000

    Adding sections

--- Main.elm
+++ Main.elm
@@ -1,3 +1,33 @@
 import Html
+import Html exposing (..)
 
-main = Html.text "hello crazy world, 2016!"
+main : Program Never Model Msg
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }
+
+type alias Model =
+  {}
+
+init : ( Model, Cmd Msg )
+init =
+  (Model, Cmd.none)
+
+
+type Msg
+    = NoOp
+
+-- How we update our Model on a given Msg?
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    NoOp ->
+      model ! []
+
+view : Model -> Html Msg
+view model =
+  div [] [text "Hello World" ]
---
+++ README.md
@@ -0,0 +1,18 @@
+# WriteNow
+## Desktop App for everyday Writing and organize your life with Todo Lists and Quick Notes
+
+### Technology stack
+- Electron
+- Elm
+- Javascript
+
+```
+// Install dependencies
+npm install
+
+// Watch files changed and start electron app
+npm run watch
+
+// Start electron app
+npm start
+```
